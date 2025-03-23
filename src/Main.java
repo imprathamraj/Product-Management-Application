37287bdcc018d141d2a165582d7121af07fcf473
imprathamraj <rjpratham05@gmail.com>
2025-03-23 13:56:23 +0530

    [Pratham Raj] Fix. main.java to add products

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("--- WELCOME TO PRODUCT MANAGEMENT APPLICATION ---");
 
-        Product product = new Product();
-        System.out.println(product);
+        ProductService productService = new ProductService();
+
+        // Hey service, add the products...
+        productService.addProduct(new Product("Asus Rog","Laptop","Desk1",2028));
+        productService.addProduct(new Product("Samsung M31","SmartPhone","Desk1",2026));
+        productService.addProduct(new Product("RealMe","EarPhone","Desk2",2024));
+        productService.addProduct(new Product("Zebronics","KeyBoard","Desk3",2025));
+        productService.addProduct(new Product("Zebronics","Mouse","Desk3",2025));
+
+        // Getting all products
+        List<Product> products = productService.getAllProduct();
+
+        // Using enhance for loop to print all product
+        for(Product p : products){
+            System.out.println(p);
+        }
     }
 }
