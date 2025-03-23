60b4c5b74b21875cf6374393924477492217cce9
imprathamraj <rjpratham05@gmail.com>
2025-03-23 13:17:17 +0530

    [Pratham Raj] Fix. main.java to show output to console.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("--- WELCOME TO PRODUCT MANAGEMENT APPLICATION ---");
 
-        String name1 = "Asus Rog";
-        String type1 = "laptop";
-        String place1 = "Desk 1";
-        int warranty1 = 2028;
-
-        String name2 = "Hp";
-        String type2 = "laptop";
-        String place2 = "Desk 2";
-        int warranty2 = 2027;
+        Product product = new Product();
+        System.out.println(product);
     }
 }
